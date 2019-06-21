adbe821b93b98795f012b267344c8c5cf3f97e3d
Faiq <faiq.kaboel@gmail.com>
2019-06-22 03:56:38 +0700

    Setup article routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+Use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +16,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('article', 'ArticleController');
