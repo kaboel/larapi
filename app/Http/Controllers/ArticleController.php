dc82a6597ebfed8b20e70d8a6a1335d837dc3db2
Faiq <faiq.kaboel@gmail.com>
2019-06-22 05:25:03 +0700

    Fixed StyleCI 2

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -85,7 +85,7 @@ class ArticleController extends Controller
      * Update the specified resource in storage.
      *
      * @param \Illuminate\Http\Request $request
-     * @param int $id
+     * @param int                      $id
      *
      * @return \Illuminate\Http\Response
      */
